6ad265380abf209ffa3e3ce586145f2154d5542c
Juan <juanortigosa99@gmail.com>
2023-10-24 18:21:22 -0300

    corrijo psb delimiter

--- fasta_from_pdb.py
+++ fasta_from_pdb.py
@@ -1,39 +1,55 @@
 from Bio import SeqIO
+from Bio.PDB import PDBParser
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import sys
 
+codigo_aminoacidos = {
+    "ALA": "A",
+    "ARG": "R",
+    "ASN": "N",
+    "ASP": "D",
+    "CYS": "C",
+    "GLU": "E",
+    "GLN": "Q",
+    "GLY": "G",
+    "HIS": "H",
+    "ILE": "I",
+    "LEU": "L",
+    "LYS": "K",
+    "MET": "M",
+    "PHE": "F",
+    "PRO": "P",
+    "SER": "S",
+    "THR": "T",
+    "TRP": "W",
+    "TYR": "Y",
+    "VAL": "V"
+}
+
 def pdb_to_fasta(pdb_file, fasta_file):
     # Leer el archivo PDB y extraer la secuencia de aminoácidos
+    parser = PDBParser(QUIET=True)
+    estructura = parser.get_structure("estructura", pdb_file)
+    model = estructura[0]
     sequence = ""
-    with open(pdb_file, 'r') as pdb_file:
-        for line in pdb_file:
-            if line.startswith("ATOM") and line[17:20] == "CA ":
-                residue = line[21:23]
-                sequence += residue
 
+    for chain in model:
+    	for residue in chain:
+    		sequence += codigo_aminoacidos[residue.get_resname()]
+  
     # Crear un registro de secuencia FASTA
-    record = SeqRecord(Seq(sequence), id="PDB_Seq")
+    record = SeqRecord(Seq(sequence), id=f"{fasta_file}")
 
     # Escribir el registro de secuencia FASTA en un archivo
     with open(fasta_file, 'w') as output_file:
         SeqIO.write(record, output_file, "fasta")
 
-
-
-#EJECUCION
 if __name__ == "__main__":
-
-    if sys.argv[1] == "-h" or sys.argv[1] == '-help' or sys.argv[1] == '-h' or sys.argv[1] == '-H':
-        print('Mira capo se usa asi esto : python programa.py archivo.pdb salida.fasta')
-    
-    
-    elif len(sys.argv) != 3:
+    if len(sys.argv) != 3:
         print("Uso: python programa.py archivo.pdb salida.fasta")
         sys.exit(1)
-    
-    else: 
-        pdb_file = sys.argv[1]
-        fasta_file = sys.argv[2]
+    pdb_file = sys.argv[1]
+    fasta_file = sys.argv[2]
+    pdb_to_fasta(pdb_file, fasta_file)
 
-        pdb_to_fasta(pdb_file, fasta_file)
--- pdb_position_delimiter.py
+++ pdb_position_delimiter.py
@@ -1,5 +1,4 @@
-#Delimitador de posiciones
-import argparse
+# Delimitador de posiciones
 from Bio.PDB import PDBParser, PDBIO, Select
 
 class RangoSelector(Select):
@@ -24,19 +23,23 @@ def seleccionar_y_guardar_rango_pdb(input_pdb, inicio, fin, output_pdb):
     io.save(output_pdb, selector)
 
 def main():
-    parser = argparse.ArgumentParser(description="Selecciona y guarda un rango de posiciones de un archivo PDB.")
-    parser.add_argument("archivo_entrada", help="Archivo PDB de entrada")
-    parser.add_argument("residuo_menor", type=int, help="Número del residuo menor en el rango")
-    parser.add_argument("residuo_mayor", type=int, help="Número del residuo mayor en el rango")
-    parser.add_argument("archivo_salida", help="Nombre del archivo de salida PDB")
+    import sys
 
-    args = parser.parse_args()
-    archivo_entrada = args.archivo_entrada
-    residuo_menor = args.residuo_menor
-    residuo_mayor = args.residuo_mayor
-    archivo_salida = args.archivo_salida
+    if len(sys.argv) != 5:
+        if sys.argv[1] in ["-h", "--help", "-h", "--help"]:
+            print("Uso: python programa.py archivo.pdb inicio fin archivo_salida")
+            sys.exit(0)
+        else:
+            print("Uso incorrecto. Para ayuda, ejecute con -h, --help, -h, o --help.")
+            sys.exit(1)
+
+    archivo_entrada = sys.argv[1]
+    residuo_menor = int(sys.argv[2])
+    residuo_mayor = int(sys.argv[3])
+    archivo_salida = sys.argv[4]
 
     seleccionar_y_guardar_rango_pdb(archivo_entrada, residuo_menor, residuo_mayor, archivo_salida)
 
 if __name__ == "__main__":
     main()
+
